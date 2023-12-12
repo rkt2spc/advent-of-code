0b62120ea92e34304a7559a67075e1d2cc70943f
Tuan Minh Nguyen <nmtuan.dev@gmail.com>
2023-12-13 03:14:32 +0800

    2023-12-1 p2

---
+++ 2023/12/1/P1.java
@@ -0,0 +1,33 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.Scanner;
+
+class P1 {
+  public static long solve(InputStream in) {
+    long result = 0;
+
+    Scanner sc = new Scanner(in);
+    while (sc.hasNext()) {
+      String line = sc.nextLine();
+
+      int lo = 0;
+      while (lo < line.length() && !Character.isDigit(line.charAt(lo)))
+        ++lo;
+
+      int hi = line.length() - 1;
+      while (hi > lo && !Character.isDigit(line.charAt(hi)))
+        --hi;
+
+      result += 10 * (line.charAt(lo) - '0') + (line.charAt(hi) - '0');
+    }
+
+    sc.close();
+    return result;
+  }
+
+  public static void main(String[] args) throws FileNotFoundException {
+    InputStream in = args.length > 0 ? new FileInputStream(args[0]) : System.in;
+    System.out.println(solve(in));
+  }
+}
---
+++ 2023/12/1/P2.java
@@ -0,0 +1,60 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.Scanner;
+
+class P2 {
+  private static final String[] nums = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+
+  private static int getDigit(String s, int index, boolean reverse) {
+    if (Character.isDigit(s.charAt(index))) {
+      return s.charAt(index) - '0';
+    }
+
+    for (int i = 0; i < nums.length; ++i) {
+      String num = nums[i];
+
+      if (s.regionMatches(reverse ? index - num.length() + 1 : index, num, 0, num.length()))
+        return i + 1;
+    }
+
+    return 0;
+  }
+
+  public static long solve(InputStream in) {
+    long result = 0;
+
+    Scanner sc = new Scanner(in);
+    while (sc.hasNext()) {
+      String line = sc.nextLine();
+
+      int lo = 0;
+      while (lo < line.length()) {
+        int digit = getDigit(line, lo, false);
+        if (digit != 0) {
+          result += digit * 10;
+          break;
+        }
+        ++lo;
+      }
+
+      int hi = line.length() - 1;
+      while (hi >= lo) {
+        int digit = getDigit(line, hi, true);
+        if (digit != 0) {
+          result += digit;
+          break;
+        }
+        --hi;
+      }
+    }
+
+    sc.close();
+    return result;
+  }
+
+  public static void main(String[] args) throws FileNotFoundException {
+    InputStream in = args.length > 0 ? new FileInputStream(args[0]) : System.in;
+    System.out.println(solve(in));
+  }
+}
